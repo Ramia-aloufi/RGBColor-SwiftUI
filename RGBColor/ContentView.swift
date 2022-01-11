e1006d23094820971dc0349fc021011355b2e453
RAM111111 <r.r.99@windowslive.com>
2022-01-11 15:50:05 +0300

    Initial Commit

---
+++ RGBColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBColor
+//
+//  Created by R on 08/06/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
