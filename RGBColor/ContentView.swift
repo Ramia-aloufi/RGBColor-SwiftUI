e6ff56fbe30514a45741e2384d4213d952b068b0
RAM111111 <r.r.99@windowslive.com>
2022-01-11 18:57:21 +0300

    Done!

--- RGBColor/ContentView.swift
+++ RGBColor/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var RedValue: Double = 0.0
+    @State var GreenValue: Double = 0.0
+    @State var BlueValue: Double = 0.0
+
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Red:\(RedValue,specifier: "%.2f")")
+            Slider(value: $RedValue, in: 0...1)
+                .padding()
+                .accentColor(Color.red)
+            Text("Green:\(GreenValue,specifier: "%.2f")")
+            Slider(value: $GreenValue, in: 0...1)
+                .padding()
+                .accentColor(Color.green)
+            Text("Blue:\(BlueValue,specifier: "%.2f")")
+            Slider(value: $BlueValue, in: 0...1)
+                .padding()
+                .accentColor(Color.blue)
+            Text("Result")
+            Rectangle()
+            .fill(Color(red: RedValue, green: GreenValue, blue: BlueValue))
+            .frame(width: 200, height: 200)
+        }
     }
 }
 
